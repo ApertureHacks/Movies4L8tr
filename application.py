6fe9afad440ce95b8e771017363a8816cc07502c
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-10-13 23:02:57 -0400

    adding search method

--- application.py
+++ application.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import urllib
 from functions import *
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
 	mdict = get_upcoming(10);
+	return render_template('main.html', mdict=mdict)
+
+@app.route('/search', methods=['GET'])
+def search_page():
+	title = request.args['_title']
+	title = urllib.quote(title)
+	print title
+	mdict = search_movie(title)
 	return render_template('main.html', mdict=mdict)
 
 if __name__ == '__main__':
