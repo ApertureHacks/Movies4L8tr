15c3e921ff945629c0ed23a9fc9d34dbe116d42b
Katie <katiepru@eden.rutgers.edu>
2012-10-14 01:34:18 -0400

    trying ajax things

--- application.py
+++ application.py
@@ -21,8 +21,7 @@ def search_page():
 @app.route('/movie/<idnumber>')
 def get_movie_by_id(idnumber):
 	mdict = get_movie(idnumber)
-	pprint(mdict)
 	return render_template('movie.html', movie=mdict)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,14 @@
+function validateForm() {
+	var name = document.forms["mainform"]["_user"].value;
+	if(name==null || name=="") {
+		alert("Name must be filled out");
+		return false;
+	}
+	var phone = document.forms["mainform"]["_phone"].value;
+	var email = document.forms["mainform"]["_email"].value;
+	if((phone==null && email==null) || (phone=="" && email=="") || (phone==null && email=="") || (phone=="" && email==null)) {
+		alert("Either email or phone must be filled out");
+		return false;
+	}
+	//$("#gobutton").addClass("btn-success");
+}
